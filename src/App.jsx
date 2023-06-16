f8c19c25e27ae0bca6b388ef0a01f843b477e2e2
Eva Dobružská (evadob) <eva.dobruzska@gmail.com>
2023-06-16 22:41:33 +0200

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export const App = () => {
+  return (
+    <>
+      <div className={"container"}>
+        <h1 className={"title"}>Galerie</h1>
+        <div className={"gallery"}>
+          <img src="/public/images/quiz-main-img-1.jpg" alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
