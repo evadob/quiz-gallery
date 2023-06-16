e4d25c06d47fb4bef122c2f84af3eae78c573d08
Eva Dobružská (evadob) <eva.dobruzska@gmail.com>
2023-06-16 23:21:51 +0200

    feat: new img

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ export const App = () => {
         <h1 className={"title"}>Galerie</h1>
         <div className={"gallery"}>
           <img src="/images/quiz-main-img-1.jpg" alt="" />
+          <img src="/images/question-img-1.jpg" alt="" />
         </div>
       </div>
     </>
