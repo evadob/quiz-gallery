42ad543f98fd82e5a2d24dc6f8521385036499e5
Eva Dobružská (evadob) <eva.dobruzska@gmail.com>
2023-06-18 12:30:41 +0200

    feat: new imgs

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,18 @@ export const App = () => {
         <div className={"gallery"}>
           <img src="/images/quiz-main-img-1.jpg" alt="" />
           <img src="/images/question-img-1.jpg" alt="" />
+          <img src="/images/question-img-2.jpg" alt="" />
+          <img src="/images/question-img-3.jpg" alt="" />
+          <img src="/images/question-img-4.jpg" alt="" />
+          <img src="/images/question-img-5.jpg" alt="" />
+          <img src="/images/question-img-6.jpg" alt="" />
+          <img src="/images/question-img-7.jpg" alt="" />
+          <img src="/images/question-img-8.jpg" alt="" />
+          <img src="/images/question-img-9.jpg" alt="" />
+          <img src="/images/question-img-10.jpg" alt="" />
+          <img src="/images/question-img-hieroglyphs.jpg" alt="" />
+          <img src="/images/question-img-papyrus.jpg" alt="" />
+          <img src="/images/question-img-pharaoh-young.jpg" alt="" />
         </div>
       </div>
     </>
